e23773d5b9d4f411a3f518685e27f7d78574b3c1
DESKTOP-AM8FIS3\admin <tung.nguyen.thac@ntq-solution.com.vn>
2018-11-09 09:33:09 +0700

    ví dụ đơn giản 1

--- App.js
+++ App.js
@@ -1,21 +1,47 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import {StyleSheet, Text, View, Animated} from 'react-native';
 
 export default class App extends React.Component {
-  render() {
-    return (
-      <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-      </View>
-    );
-  }
+    constructor(props) {
+        super(props);
+        this.state = {
+            fetOpacity: new Animated.Value(0)
+        };
+    }
+
+    componentDidMount() {
+        Animated.timing(this.state.fetOpacity,
+            {
+                toValue: 1,
+                duration: 5000
+            }
+        ).start()
+    }
+
+    render() {
+
+        return (
+            <View style={styles.container}>
+                <Animated.View style={{
+                    width: 300,
+                    height: 300,
+                    backgroundColor: '#2132ff',
+                    justifyContent: 'center',
+                    alignItems: 'center',
+                    opacity:this.state.fetOpacity
+                }}>
+                    <Text style={{color: '#fff'}}>Animation đơn giản</Text>
+                </Animated.View>
+            </View>
+        );
+    }
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    container: {
+        flex: 1,
+        backgroundColor: '#fff',
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
 });
